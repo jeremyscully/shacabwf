05bf016bd39a730362b14e74d613afb311d981c9
Jeremy Scully <user@example.com>
2025-03-10 15:22:12 -0700

    Disable SPA proxy and set default route to Home/Simple

--- ShacabWf.Web/Program.cs
+++ ShacabWf.Web/Program.cs
@@ -58,6 +58,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Simple}/{id?}");
 
 app.Run();
